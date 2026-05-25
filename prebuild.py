7e38abfd73502eefe36dd2817f48437132148893
Muaath <me@muaath.dev>
2026-05-25 22:50:33 +0300

    Add initial

---
+++ prebuild.py
@@ -0,0 +1,11 @@
+import requests
+
+GITHUB_REPO = 'https://github.com/informatics-sa/WebsiteBuilder'
+
+
+# prebuild.py:
+# - Clone all files from GITHUB_REPO to root directory
+# Worklfow:
+# - Run build.py
+# build.py:
+# - Builds everything else
